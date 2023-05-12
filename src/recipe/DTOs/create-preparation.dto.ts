1d643e1cd8ecefcf1b50c77f794df8a150bdba16
Marysia Biss <mariabiss3012@gmail.com>
2023-05-12 22:26:03 +0200

    adding missing validation

--- src/recipe/DTOs/create-preparation.dto.ts
+++ src/recipe/DTOs/create-preparation.dto.ts
@@ -1,4 +1,9 @@
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreatePreparingDTO {
+  @IsString()
   step: string;
+
+  @IsNumber()
   order: number;
 }
--- src/recipe/DTOs/create-recipe.dto.ts
+++ src/recipe/DTOs/create-recipe.dto.ts
@@ -5,10 +5,14 @@ import { CreateIngredientDTO } from './create-ingredient.dto';
 export class CreateRecipeDTO {
   @IsString()
   name: string;
+
   @IsString()
   description: string;
+
   @IsString()
   imageURL: string;
+
   preparing: CreatePreparingDTO[];
+
   ingredients: CreateIngredientDTO[];
 }
