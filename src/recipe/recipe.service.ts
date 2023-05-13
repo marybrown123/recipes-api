c3d4c42e997a8321ff9943814bda17768cbd5b13
Marysia Biss <mariabiss3012@gmail.com>
2023-05-13 14:19:09 +0200

    an endpoint for updating recipe

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateRecipeDTO } from './DTOs/create-recipe.dto';
 import { RecipeResponse } from './responses/recipe.response';
+import { UpdateRecipeDTO } from './DTOs/update-recipe.dto';
 
 @Injectable()
 export class RecipeService {
@@ -27,5 +28,67 @@ export class RecipeService {
       },
     });
     return new RecipeResponse(newRecipe);
+  }
+
+  async updateRecipe(recipeId: number, newRecipe: UpdateRecipeDTO) {
+    const recipeToUpdate = await this.prisma.recipe.findUnique({
+      where: {
+        id: recipeId,
+      },
+    });
+
+    if (!recipeToUpdate) {
+      throw new HttpException(
+        'There is no recipe with this is',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    if (newRecipe.preparing) {
+      await this.prisma.recipePreparationSteps.deleteMany({
+        where: {
+          recipeId,
+        },
+      });
+    }
+
+    if (newRecipe.ingredients) {
+      await this.prisma.recipeIngredients.deleteMany({
+        where: {
+          recipeId,
+        },
+      });
+    }
+
+    const updatedRecipe = await this.prisma.recipe.update({
+      where: { id: recipeId },
+      data: {
+        name: newRecipe.name,
+        description: newRecipe.description,
+        imageURL: newRecipe.imageURL,
+        preparing: { create: newRecipe.preparing },
+        ingredients: { create: newRecipe.ingredients },
+      },
+      include: {
+        preparing: true,
+        ingredients: true,
+      },
+    });
+
+    return new RecipeResponse(updatedRecipe);
+  }
+
+  async findOneRecipe(recipeId: number) {
+    const recipeFromDb = await this.prisma.recipe.findUnique({
+      where: {
+        id: recipeId,
+      },
+      include: {
+        preparing: true,
+        ingredients: true,
+      },
+    });
+
+    return new RecipeResponse(recipeFromDb);
   }
 }
---
+++ src/user/guards/is-user-author.guard.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { RecipeService } from 'src/recipe/recipe.service';
+
+@Injectable()
+export class IsUserAuthorGuard implements CanActivate {
+  constructor(private recipeService: RecipeService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const recipeFromDb = await this.recipeService.findOneRecipe(
+      Number(request.params.id),
+    );
+
+    if (request.user.id !== recipeFromDb.authorId) {
+      throw new HttpException(
+        'User do not own this recipe',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    return true;
+  }
+}
