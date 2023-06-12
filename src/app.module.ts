b798e20b8cdc6f718b2117fd42027f678c8b0313
Maria Biss <mariabiss@MacBook-Pro-Maria.local>
2023-06-12 04:59:54 -0700

    fix: make env work

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { RecipeModule } from './recipe/recipe.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, AuthModule, RecipeModule],
+  imports: [UserModule, AuthModule, RecipeModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
