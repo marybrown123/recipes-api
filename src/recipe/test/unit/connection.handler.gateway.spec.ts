90495ffcd65d0c6bde9be40d8bead5e6d19966b2
Marysia <mariabiss3012@gmail.com>
2023-08-01 12:34:08 +0200

    fix: add new test cases and fix existing ones

--- src/recipe/test/unit/connection.handler.gateway.spec.ts
+++ src/recipe/test/unit/connection.handler.gateway.spec.ts
@@ -20,6 +20,8 @@ const socket: Socket = {
   },
 };
 
+const invalidTokenError = new Error('invalid token');
+
 describe('Connection Handler Gateway', () => {
   let connectionHandlerGateway: ConnectionHandlerGateway;
   let authService: AuthService;
@@ -36,7 +38,7 @@ describe('Connection Handler Gateway', () => {
     authService = module.get<AuthService>(AuthService);
   });
 
-  afterEach(async () => {
+  afterEach(() => {
     jest.clearAllMocks();
     connectionHandlerGateway.connectedUsers.clear();
   });
@@ -56,9 +58,35 @@ describe('Connection Handler Gateway', () => {
     const connectedUsers = connectionHandlerGateway.connectedUsers;
 
     expect(authServiceVerifyToken).toBeCalledTimes(1);
+    expect(authServiceVerifyToken).toBeCalledWith(
+      socket.handshake.headers.authorization,
+    );
     expect(connectedUsersSet).toBeCalledTimes(1);
+    expect(connectedUsersSet).toBeCalledWith(verifiedToken.sub, socket.id);
     expect(connectedUsers.size).toBe(1);
     expect(connectedUsers.get(verifiedToken.sub)).toBe(socket.id);
+  });
+
+  it('should throw an error when token is invalid while trying to add connected user to connectedUsers', async () => {
+    const authServiceVerifyToken = jest
+      .spyOn(authService, 'verifyToken')
+      .mockRejectedValue(invalidTokenError);
+
+    const connectedUsersSet = jest.spyOn(
+      connectionHandlerGateway.connectedUsers,
+      'set',
+    );
+
+    await connectionHandlerGateway.handleConnection(socket);
+
+    const connectedUsers = connectionHandlerGateway.connectedUsers;
+
+    expect(authServiceVerifyToken).toBeCalledTimes(1);
+    expect(authServiceVerifyToken).toBeCalledWith(
+      socket.handshake.headers.authorization,
+    );
+    expect(connectedUsersSet).toBeCalledTimes(0);
+    expect(connectedUsers.size).toBe(0);
   });
 
   it('should remove disconnected user from connectedUsers', async () => {
@@ -71,12 +99,42 @@ describe('Connection Handler Gateway', () => {
       'delete',
     );
 
+    connectionHandlerGateway.connectedUsers.set(verifiedToken.sub, socket.id);
+
     await connectionHandlerGateway.handleDisconnect(socket);
 
     const connectedUsers = connectionHandlerGateway.connectedUsers;
 
     expect(authServiceVerifyToken).toBeCalledTimes(1);
+    expect(authServiceVerifyToken).toBeCalledWith(
+      socket.handshake.headers.authorization,
+    );
     expect(connectedUsersDelete).toBeCalledTimes(1);
+    expect(connectedUsersDelete).toBeCalledWith(verifiedToken.sub);
     expect(connectedUsers.size).toBe(0);
+  });
+
+  it('should throw an error when token is invalid while trying to delete disconnected user from connectedUsers', async () => {
+    const authServiceVerifyToken = jest
+      .spyOn(authService, 'verifyToken')
+      .mockRejectedValue(invalidTokenError);
+
+    const connectedUsersDelete = jest.spyOn(
+      connectionHandlerGateway.connectedUsers,
+      'delete',
+    );
+
+    connectionHandlerGateway.connectedUsers.set(verifiedToken.sub, socket.id);
+
+    await connectionHandlerGateway.handleDisconnect(socket);
+
+    const connectedUsers = connectionHandlerGateway.connectedUsers;
+
+    expect(authServiceVerifyToken).toBeCalledTimes(1);
+    expect(authServiceVerifyToken).toBeCalledWith(
+      socket.handshake.headers.authorization,
+    );
+    expect(connectedUsersDelete).toBeCalledTimes(0);
+    expect(connectedUsers.size).toBe(1);
   });
 });
