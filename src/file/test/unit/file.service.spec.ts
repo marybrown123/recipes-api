ed979f63e26eed2224de47421a43d56e05c3115f
Marysia <mariabiss3012@gmail.com>
2023-08-18 13:32:32 +0200

    fix: tests

--- src/file/test/unit/file.service.spec.ts
+++ src/file/test/unit/file.service.spec.ts
@@ -2,10 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../../app.module';
 import { FileService } from '../../file.service';
 import { CommandBus, CqrsModule, QueryBus } from '@nestjs/cqrs';
-import { FileResponse } from '../../responses/file.response';
 import { S3Service } from '../../s3.service';
 import { S3ServiceMock } from '../mocks/s3.service.mock';
-import { PrismaService } from '../../../prisma/prisma.service';
 
 const fileName = 'testName';
 
@@ -13,8 +11,6 @@ describe('File Service', () => {
   let fileService: FileService;
   let commandBus: CommandBus;
   let queryBus: QueryBus;
-  let testFile: FileResponse;
-  let prismaService: PrismaService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,20 +24,11 @@ describe('File Service', () => {
     fileService = module.get<FileService>(FileService);
     commandBus = module.get<CommandBus>(CommandBus);
     queryBus = module.get<QueryBus>(QueryBus);
-    prismaService = module.get<PrismaService>(PrismaService);
-  });
-
-  beforeEach(async () => {
-    testFile = await fileService.createFile(fileName);
   });
 
   afterEach(async () => {
     jest.clearAllMocks();
     jest.restoreAllMocks();
-  });
-
-  afterAll(async () => {
-    await prismaService.file.deleteMany();
   });
 
   it('should create new file', async () => {
@@ -51,22 +38,28 @@ describe('File Service', () => {
 
     expect(result.name).toBe('testName');
     expect(commandBusExecute).toBeCalledTimes(1);
+
+    await fileService.deleteFile(result.id);
   });
 
   it('should find file by id', async () => {
     const queryBusExecute = jest.spyOn(queryBus, 'execute');
+    const testFile = await fileService.createFile(fileName);
 
     const result = await fileService.findFileById(testFile.id);
 
     expect(result.name).toBe('testName');
     expect(queryBusExecute).toBeCalledTimes(1);
+
+    await fileService.deleteFile(testFile.id);
   });
 
   it('should delete file', async () => {
     const commandBusExecute = jest.spyOn(commandBus, 'execute');
+    const testFile = await fileService.createFile(fileName);
 
     await fileService.deleteFile(testFile.id);
 
-    expect(commandBusExecute).toBeCalledTimes(1);
+    expect(commandBusExecute).toBeCalledTimes(2);
   });
 });
