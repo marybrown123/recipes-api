977da960c47d1e8f58411fe1e71cfef138e57944
Marysia <mariabiss3012@gmail.com>
2023-09-01 11:52:46 +0200

    fix: change return in isUserVerifiedGuard

--- src/user/guards/is-user-verified.guard.ts
+++ src/user/guards/is-user-verified.guard.ts
@@ -4,10 +4,6 @@ export class IsUserVerifiedGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
-    if (!request.user.isVerified) {
-      return false;
-    }
-
-    return true;
+    return request.user.isVerified;
   }
 }
