a87a41c5924296948d2a46b69e2c170f2aac5b4f
Marysia <mariabiss3012@gmail.com>
2023-09-07 13:41:12 +0200

    fix: use httpModule instead of axios

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [WebhookService],
   exports: [WebhookService],
 })
--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -1,17 +1,23 @@
-import axios from 'axios';
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
 import { RecipeResponse } from 'src/recipe/responses/recipe.response';
+import { lastValueFrom, map } from 'rxjs';
 
+@Injectable()
 export class WebhookService {
+  constructor(private readonly httpService: HttpService) {}
+
   async sendWebhookWithRecipe(recipe: RecipeResponse): Promise<void> {
     const webhookURL = process.env.WEBHOOK_URL;
-    const webhookData = {
-      ...recipe,
-    };
 
     try {
-      await axios.post(webhookURL, webhookData);
+      await lastValueFrom(
+        this.httpService
+          .post(webhookURL, recipe)
+          .pipe(map((response) => response.data)),
+      );
     } catch (error) {
-      throw Error('There was an error while sending a request');
+      throw new Error('There was an error while sending request');
     }
   }
 }
